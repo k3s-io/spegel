9466a55293301075a2d1613a3744c6bf45342e8b
Brad Davidson <brad.davidson@rancher.com>
2024-01-09 00:47:35 +0000

    Always include digest in image string
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/oci/image.go
+++ pkg/oci/image.go
@@ -62,10 +62,7 @@ func (i Image) String() string {
 	if i.Tag != "" {
 		tag = fmt.Sprintf(":%s", i.Tag)
 	}
-	dgstStr := ""
-	if i.Tag != "" {
-		dgstStr = fmt.Sprintf("@%s", i.Digest.String())
-	}
+	dgstStr := fmt.Sprintf("@%s", i.Digest.String())
 	return fmt.Sprintf("%s/%s%s%s", i.Registry, i.Repository, tag, dgstStr)
 }
 
